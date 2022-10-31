12ba55cb67666aef0c07e1db034b4ae6c1674232
Tunji Mubarak AZEEZ <azeeztim@gmail.com>
2022-10-31 15:13:16 +0100

    added API

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,139 +1,46 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useStore, useWatch$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import { Link } from '@builder.io/qwik-city';
+
 
 export default component$(() => {
+  
+const myCity = useStore({
+  city: "",
+});
+
+const myWeather = useStore({
+  
+})
+
+const state = useStore({
+  url: `https://api.openweathermap.org/data/2.5/weather?q=${myCity.city}&units=imperial&appid=${ATM_KEY}`
+  ,
+  responseJson: undefined,
+});
+
+
+
+
+  const ATM_KEY = "3b762aabb1ab28c97b70d949ba859ad4"
+  //  const url =  `https://api.openweathermap.org/data/2.5/weather?q=${myCity.city}&units=imperial&appid=${ATM_KEY}`
+   
+
+
+   useWatch$(async ({ track }) =>{
+    track(() =>state.url);
+    const res = await fetch(state.url);
+    const json = await res.json();
+    state.responseJson = json;
+    
+   })
   return (
+    
     <div>
-      <h1>
-        Welcome to Qwik <span class="lightning">⚡️</span>
-      </h1>
-
-      <ul>
-        <li>
-          Check out the <code>src/routes</code> directory to get started.
-        </li>
-        <li>
-          Add integrations with <code>npm run qwik add</code>.
-        </li>
-        <li>
-          More info about development in <code>README.md</code>
-        </li>
-      </ul>
-
-      <h2>Commands</h2>
-
-      <table class="commands">
-        <tr>
-          <td>
-            <code>npm run dev</code>
-          </td>
-          <td>Start the dev server and watch for changes.</td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run preview</code>
-          </td>
-          <td>Production build and start preview server.</td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run build</code>
-          </td>
-          <td>Production build.</td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add</code>
-          </td>
-          <td>Select an integration to add.</td>
-        </tr>
-      </table>
-
-      <h2>Add Integrations</h2>
-
-      <table class="commands">
-        <tr>
-          <td>
-            <code>npm run qwik add cloudflare-pages</code>
-          </td>
-          <td>
-            <a href="https://developers.cloudflare.com/pages" target="_blank">
-              Cloudflare Pages Server
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add express</code>
-          </td>
-          <td>
-            <a href="https://expressjs.com/" target="_blank">
-              Nodejs Express Server
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add netlify-edge</code>
-          </td>
-          <td>
-            <a href="https://docs.netlify.com/" target="_blank">
-              Netlify Edge Functions
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add static-node</code>
-          </td>
-          <td>
-            <a
-              href="https://qwik.builder.io/qwikcity/static-site-generation/overview/"
-              target="_blank"
-            >
-              Static Site Generation (SSG)
-            </a>
-          </td>
-        </tr>
-      </table>
-
-      <h2>Community</h2>
-
-      <ul>
-        <li>
-          <span>Questions or just want to say hi? </span>
-          <a href="https://qwik.builder.io/chat" target="_blank">
-            Chat on discord!
-          </a>
-        </li>
-        <li>
-          <span>Follow </span>
-          <a href="https://twitter.com/QwikDev" target="_blank">
-            @QwikDev
-          </a>
-          <span> on Twitter</span>
-        </li>
-        <li>
-          <span>Open issues and contribute on </span>
-          <a href="https://github.com/BuilderIO/qwik" target="_blank">
-            Github
-          </a>
-        </li>
-        <li>
-          <span>Watch </span>
-          <a href="https://qwik.builder.io/media/" target="_blank">
-            Presentations, Podcasts, Videos, etc.
-          </a>
-        </li>
-      </ul>
-      <Link class="mindblow" href="/flower">
-        Blow my mind 🤯
-      </Link>
+    <h1></h1>
     </div>
   );
 });
 
-export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
-};
+// export const head: DocumentHead = {
+//   title: 'Welcome to Qwik',
+// };
